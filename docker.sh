3981e7a380cff5dbbc163e832b5ba18b6394049f
chandu <chandu@devops.com>
2024-03-06 17:22:20 -0600

    ok

--- docker.sh
+++ docker.sh
@@ -1,8 +1,10 @@
-#!/bin/bash/
+#!/bin/bash
 
 sudo yum install -y yum-utils
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl start docker
 

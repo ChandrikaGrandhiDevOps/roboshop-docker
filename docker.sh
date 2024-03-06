d40213d6cded2e092f842f857edcb9c08b599c06
chandu <chandu@devops.com>
2024-03-06 17:16:36 -0600

    ok

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+usermod -aG docker centos
